068b6bb009109b527153f0815049d3622d06262f
Forest Agostinelli <foresta@cse.sc.edu>
2022-02-04 19:31:51 -0500

    coding hw 2

---
+++ coding_hw/coding_hw2.py
@@ -0,0 +1,10 @@
+from environments.connect_four import ConnectFourState, ConnectFour
+import numpy as np
+
+
+def heuistic(state: ConnectFourState) -> float:
+    pass
+
+
+def heuristic_minimax_search(state: ConnectFourState, env: ConnectFour, depth: int) -> int:
+    pass
