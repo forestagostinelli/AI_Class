bd3fa1a6ed5598538aa0f33c3deb9238c4ef2bd2
Forest Agostinelli <foresta@cse.sc.edu>
2022-04-07 23:47:52 -0400

    coding hw grad

---
+++ coding_hw/coding_hw_grad.py
@@ -0,0 +1,13 @@
+from typing import Tuple
+import torch.nn as nn
+import numpy as np
+
+
+def train_autoencoder(data_input_np: np.ndarray) -> Tuple[nn.Module, nn.Module]:
+    """
+
+    :param data_input_np: training inputs
+    :return: trained encoder and decoder
+    """
+
+    pass
